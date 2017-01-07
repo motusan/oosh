6314853b47c3925f64b5fa5a587c7b7db80dd30b
motusan <scream@bloodfreak.ca>
2017-01-06 22:17:22 -0800

    receonfigure keyboard synth to have 2 rows of keys instead of one

--- assets/js/app/widgets/KeyboardSynth/widget.js
+++ assets/js/app/widgets/KeyboardSynth/widget.js
@@ -9,9 +9,10 @@ define({
             "event":{
                 "name": "oosh.keydown",
                 "properties":{
-                    ":detail:properties:code": ["KeyA", "KeyW", "KeyS", "KeyE", "KeyD", "KeyF",
-                            "KeyT", "KeyG", "KeyY", "KeyH", "KeyU", "KeyJ", "KeyK", "KeyO",
-                            "KeyL", "KeyP", "Semicolon", "Quote"]
+                    ":detail:properties:code": ["Digit2","Digit3", "Digit4","Digit5","Digit6", "Digit8","Digit9","Minus", "Equal",
+							"KeyQ","KeyW","KeyE","KeyR","KeyT","KeyY","KeyU","KeyI","KeyO","KeyP","BracketLeft","BracketRight",
+							"KeyS","KeyD", "KeyG","KeyH","KeyJ", "KeyL","Semicolon",
+                            "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma","Period","Slash"]
                 }
             },
             "targets": [{
@@ -28,14 +29,16 @@ define({
                         "input" : ":event:detail:properties:code",
                         "transform":[
                             {
-                                "map": {
-                                    "KeyA" : 1, "KeyW" : 2, "KeyS" : 3, "KeyE" : 4, "KeyD" : 5,
-                                    "KeyF" : 6, "KeyT" : 7 , "KeyG" : 8, "KeyY" : 9, "KeyH" : 10,
-                                    "KeyU" : 11, "KeyJ" : 12, "KeyK" : 13, "KeyO" : 14, "KeyL" : 15,
-                                    "KeyP" : 16, "Semicolon" : 17, "Quote" : 18
-                                }
+								"map":{
+									"KeyQ" : 1, "Digit2" : 2, "KeyW" : 3, "Digit3" : 4, "KeyE" : 5,
+									"KeyR" : 6, "Digit5" : 7, "KeyT" : 8, "Digit6" : 9, "KeyY" : 10, "Digit7" : 11, "KeyU" : 12,
+									"KeyI" : 13, "Digit9" : 14, "KeyO" : 15, "Digit0" : 16, "KeyP" : 17, "BracketLeft" : 18, "Plus" : 19, "BracketRight" : 20,
+									"KeyZ" : 25, "KeyS" : 26, "KeyX" : 27, "KeyD" : 28, "KeyC" : 29,
+									"KeyV" : 30, "KeyG" : 31, "KeyB" : 32, "KeyH" : 33, "KeyN" : 34, "KeyJ" : 35, "KeyM" : 36,
+									"Comma" : 37, "KeyL" : 38, "Period" : 39, "Semicolon" : 40, "Slash" : 41
+								}
                             },
-                            {"add": 2},
+                            {"subtract": 26},
                             {"exponentOf": 1.059463094359},
                             {"multiply": 440}
                         ]
@@ -48,9 +51,10 @@ define({
             "event":{
                 "name": "oosh.keyup",
                 "properties":{
-                    ":detail:properties:code": ["KeyA", "KeyW", "KeyS", "KeyE", "KeyD", "KeyF",
-                            "KeyT", "KeyG", "KeyY", "KeyH", "KeyU", "KeyJ", "KeyK", "KeyO",
-                            "KeyL", "KeyP", "Semicolon", "Quote"]
+                    ":detail:properties:code": ["Digit2","Digit3", "Digit4","Digit5","Digit6", "Digit8","Digit9","Minus", "Equal",
+							"KeyQ","KeyW","KeyE","KeyR","KeyT","KeyY","KeyU","KeyI","KeyO","KeyP","BracketLeft","BracketRight",
+							"KeyS","KeyD", "KeyG","KeyH","KeyJ", "KeyL","Semicolon",
+                            "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma","Period","Slash"]
                 }
             },
             "targets": [{
